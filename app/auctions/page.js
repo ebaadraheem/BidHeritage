463252237ff51755c1b1a7573b1b98ec986fd18c
Ebaad Raheem <143934811+ebaadraheem@users.noreply.github.com>
2024-07-03 20:09:03 +0500

    page.js

--- app/auctions/page.js
+++ app/auctions/page.js
@@ -69,11 +69,12 @@ const Auction = () => {
     const fetchCategories = async () => {
       try {
         const response = await fetch('/api/category/all', {
-          method: 'GET',
+          method: 'POST',
           headers: {
-            'Content-Type': 'application/json',
+              'Content-Type': 'application/json',
           },
-        });
+          body: JSON.stringify({message:"Hello" }),
+      });
         const data = await response.json();
         if (data.success) {
           setcategories(data.categories);
