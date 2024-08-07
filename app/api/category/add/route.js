cfae2776b616795e104e7d5ea4ee69132ffdbc98
Ebaad Raheem <143934811+ebaadraheem@users.noreply.github.com>
2024-08-07 22:54:36 +0500

    route.js

--- app/api/category/add/route.js
+++ app/api/category/add/route.js
@@ -1,8 +1,9 @@
 import { NextResponse } from "next/server";
 import Category from "@/app/models/Category";
-
+import connectDB from "@/app/lib/mongodb";
 // POST method to add a category
 export async function POST(req) {
+    await connectDB()
     try {
         const body = await req.json();
         const { category } = body;
