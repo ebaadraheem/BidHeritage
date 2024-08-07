89f5ee1c375677e08f8b6362512ee505faf86569
Ebaad Raheem <143934811+ebaadraheem@users.noreply.github.com>
2024-08-07 22:55:09 +0500

    route.js

--- app/api/category/all/route.js
+++ app/api/category/all/route.js
@@ -1,7 +1,9 @@
 
 import { NextResponse } from "next/server";
 import Category from "@/app/models/Category";
+import connectDB from "@/app/lib/mongodb";
 export async function POST(req) {
+    await connectDB()
     try {
         const messages=await req.json()
         console.log("Message is ",messages)
