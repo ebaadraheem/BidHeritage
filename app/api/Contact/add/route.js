7d48ab5394ae2eb1546408dea5e5370eee8be252
Ebaad Raheem <143934811+ebaadraheem@users.noreply.github.com>
2024-08-07 22:36:31 +0500

    route.js

--- app/api/Contact/add/route.js
+++ app/api/Contact/add/route.js
@@ -1,7 +1,9 @@
 import ContactForm from "@/app/models/ContactForm";
 import { NextResponse } from "next/server";
+import connectDB from "@/app/lib/mongodb";
 
 export async function POST(req) {
+    await connectDB()
     try {
         const body = await req.json();
         const dt = await ContactForm.create(body);
