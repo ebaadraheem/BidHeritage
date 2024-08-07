056c202e24e55876a66bf49a8d03d55e1b0feb1e
Ebaad Raheem <143934811+ebaadraheem@users.noreply.github.com>
2024-08-07 22:58:57 +0500

    route.js

--- app/api/Bidders/getbidders/route.js
+++ app/api/Bidders/getbidders/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import Bidders from "@/app/models/Bidders";
+import connectDB from "@/app/lib/mongodb";
 export async function POST(req) {
+    await connectDB()
     try {
         const fetchdata = await req.json();
         const dt = await Bidders.find({ specificId: { $in: fetchdata.Id } });
