f37cc8198baa60acc66a6fde763c2f744c53c109
Ebaad Raheem <143934811+ebaadraheem@users.noreply.github.com>
2024-09-10 19:21:20 +0500

    page.js

--- app/auctions/page.js
+++ app/auctions/page.js
@@ -158,7 +158,7 @@ const Auction = () => {
           <div className="text-2xl mx-4 md:mx-6 max-md:text-xl lg:hidden font-semibold">Available</div>
           <div className={`flexer  ${auctionsdata.length === 0 && "min-h-[80vh]"}`} >
             {auctionsdata.length === 0 && <div className=' w-full font-semibold flexer h-[75vh] '>
-               <svg className="w-8 h-8 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><linearGradient id="a11"><stop offset="0" stop-color="#000000" stop-opacity="0"></stop><stop offset="1" stop-color="#000000"></stop></linearGradient><circle fill="none" stroke="url(#a11)" stroke-width="15" stroke-linecap="round" stroke-dasharray="0 44 0 44 0 44 0 44 0 360" cx="100" cy="100" r="70" transform-origin="center"><animateTransform type="rotate" attributeName="transform" calcMode="discrete" dur="1.4" values="360;324;288;252;216;180;144;108;72;36" repeatCount="indefinite"></animateTransform></circle></svg>
+              <div>No items to show</div> 
               </div>}
 
             <div className=' flex-wrap max-sm:p-1 sm:p-4  flex items-center  max-w-[1800px] justify-center max-md:flex-col '>
